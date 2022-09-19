b4640d7fbfb0e65ccdc73c1fe6d5c803eb889bae
AlenaGM <alena.guillaume4@gmail.com>
2022-09-19 13:27:18 +0200

    added img

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,154 +2,155 @@ let modelPrice = 0;
 let yearPrice = 1;
 let mileagePrice = 0;
 
-const formatter = new Intl.NumberFormat('ru');
+const formatter = new Intl.NumberFormat("ru");
 
-const selects = document.querySelectorAll('select')
-const inputs = document.querySelectorAll('input')
+const selects = document.querySelectorAll("select");
+const inputs = document.querySelectorAll("input");
 
-// селекты
-document.getElementById('carModel').addEventListener('change', function() {
-    modelPrice = this.value;
+//selects
+document.getElementById("carModel").addEventListener("change", function () {
+  modelPrice = this.value;
 });
 
-document.getElementById('carYear').addEventListener('change', function() {
-    yearPrice = this.value;
+document.getElementById("carYear").addEventListener("change", function () {
+  yearPrice = this.value;
 });
 
-document.getElementById('mileage').addEventListener('change', function() {;
-    mileagePrice = this.value*10;
+document.getElementById("mileage").addEventListener("change", function () {
+  mileagePrice = this.value * 10;
 });
 
-// радио кнопки
+//radio buttons
 const radioMotor = document.querySelectorAll('input[name="motor"]');
-const radioTransmission = document.querySelectorAll('input[name="transmission"]');
+const radioTransmission = document.querySelectorAll(
+  'input[name="transmission"]'
+);
 const radioGearbox = document.querySelectorAll('input[name="gearbox"]');
 
-
-// чекбоксы
-const powerSteering = document.getElementById('powerSteering');
-const audioSystem = document.getElementById('audioSystem');
-const tripComputer = document.getElementById('tripComputer');
-const parkingSensor = document.getElementById('parkingSensor');
-const rearСamera = document.getElementById('rearСamera');
-const climateControl = document.getElementById('climateControl');
-const cruiseControl = document.getElementById('cruiseControl');
-const infotainment = document.getElementById('infotainment');
-const rearWindowHeating = document.getElementById('rearWindowHeating');
-const windscreenHeating = document.getElementById('windscreenHeating');
-const mirrorsHeating = document.getElementById('mirrorsHeating');
-const seatsHeating = document.getElementById('seatsHeating');
-const parkingAssist = document.getElementById('parkingAssist');
-const remoteStart = document.getElementById('remoteStart');
+//checkboxes
+const powerSteering = document.getElementById("powerSteering");
+const audioSystem = document.getElementById("audioSystem");
+const tripComputer = document.getElementById("tripComputer");
+const parkingSensor = document.getElementById("parkingSensor");
+const rearСamera = document.getElementById("rearСamera");
+const climateControl = document.getElementById("climateControl");
+const cruiseControl = document.getElementById("cruiseControl");
+const infotainment = document.getElementById("infotainment");
+const rearWindowHeating = document.getElementById("rearWindowHeating");
+const windscreenHeating = document.getElementById("windscreenHeating");
+const mirrorsHeating = document.getElementById("mirrorsHeating");
+const seatsHeating = document.getElementById("seatsHeating");
+const parkingAssist = document.getElementById("parkingAssist");
+const remoteStart = document.getElementById("remoteStart");
 
 function calculate() {
-    //Базовая цена модели, потеря стоимости с годами и км (селекты и инпут)
-    let totalPrice = modelPrice * yearPrice - mileagePrice;
+  //base price, depreciation with years and km (selects and input)
+  let totalPrice = modelPrice * yearPrice - mileagePrice;
 
-    //Влияние типа двигателя, привода, кор.передач на базовую цену (радиокнопки)
-    for (const radio of radioMotor) {
-        if (radio.checked === true) {
-            totalPrice = totalPrice * radio.value;
-        }
+  //base price evolution depending on engine type, gear and transmission (radio buttons)
+  for (const radio of radioMotor) {
+    if (radio.checked === true) {
+      totalPrice = totalPrice * radio.value;
     }
+  }
 
-    for (const transmission of radioTransmission) {
-        if (transmission.checked === true) {
-            totalPrice = totalPrice + +transmission.value;
-        }
+  for (const transmission of radioTransmission) {
+    if (transmission.checked === true) {
+      totalPrice = totalPrice + +transmission.value;
     }
+  }
 
-    for (const gearbox of radioGearbox) {
-        if (gearbox.checked === true) {
-            totalPrice = totalPrice + +gearbox.value;
-        }
+  for (const gearbox of radioGearbox) {
+    if (gearbox.checked === true) {
+      totalPrice = totalPrice + +gearbox.value;
     }
+  }
 
-    //Увеличение цены при выборе опций (чекбоксы)
-    if (powerSteering.checked === true) {
-        totalPrice = totalPrice + +powerSteering.value;
-    }
+  //price increase depending on selected options (checkboxes)
+  if (powerSteering.checked === true) {
+    totalPrice = totalPrice + +powerSteering.value;
+  }
 
-    if (audioSystem.checked === true) {
-        totalPrice = totalPrice + +audioSystem.value;
-    }
+  if (audioSystem.checked === true) {
+    totalPrice = totalPrice + +audioSystem.value;
+  }
 
-    if (tripComputer.checked === true) {
-        totalPrice = totalPrice + +tripComputer.value;
-    }
+  if (tripComputer.checked === true) {
+    totalPrice = totalPrice + +tripComputer.value;
+  }
 
-    if (parkingSensor.checked === true) {
-        totalPrice = totalPrice + +parkingSensor.value;
-    }
+  if (parkingSensor.checked === true) {
+    totalPrice = totalPrice + +parkingSensor.value;
+  }
 
-    if (rearСamera.checked === true) {
-        totalPrice = totalPrice + +rearСamera.value;
-    }
+  if (rearСamera.checked === true) {
+    totalPrice = totalPrice + +rearСamera.value;
+  }
 
-    if (climateControl.checked === true) {
-        totalPrice = totalPrice + +climateControl.value;
-    }
+  if (climateControl.checked === true) {
+    totalPrice = totalPrice + +climateControl.value;
+  }
 
-    if (cruiseControl.checked === true) {
-        totalPrice = totalPrice + +cruiseControl.value;
-    }
+  if (cruiseControl.checked === true) {
+    totalPrice = totalPrice + +cruiseControl.value;
+  }
 
-    if (infotainment.checked === true) {
-        totalPrice = totalPrice + +infotainment.value;
-    }
+  if (infotainment.checked === true) {
+    totalPrice = totalPrice + +infotainment.value;
+  }
 
-    if (rearWindowHeating.checked === true) {
-        totalPrice = totalPrice + +rearWindowHeating.value;
-    }
+  if (rearWindowHeating.checked === true) {
+    totalPrice = totalPrice + +rearWindowHeating.value;
+  }
 
-    if (windscreenHeating.checked === true) {
-        totalPrice = totalPrice + +windscreenHeating.value;
-    }
+  if (windscreenHeating.checked === true) {
+    totalPrice = totalPrice + +windscreenHeating.value;
+  }
 
-    if (mirrorsHeating.checked === true) {
-        totalPrice = totalPrice + +mirrorsHeating.value;
-    }
+  if (mirrorsHeating.checked === true) {
+    totalPrice = totalPrice + +mirrorsHeating.value;
+  }
 
-    if (seatsHeating.checked === true) {
-        totalPrice = totalPrice + +seatsHeating.value;
-    }
+  if (seatsHeating.checked === true) {
+    totalPrice = totalPrice + +seatsHeating.value;
+  }
 
-    if (parkingAssist.checked === true) {
-        totalPrice = totalPrice + +parkingAssist.value;
-    }
+  if (parkingAssist.checked === true) {
+    totalPrice = totalPrice + +parkingAssist.value;
+  }
 
-    if (remoteStart.checked === true) {
-        totalPrice = totalPrice + +remoteStart.value;
-    }
+  if (remoteStart.checked === true) {
+    totalPrice = totalPrice + +remoteStart.value;
+  }
 
-    // Показывает промежуточную цену
-    // Если цена ушла в минус (пример, слишком большой км -> показывать 0)
-    if(totalPrice > 0){
-        document.getElementById('totalPrice').innerHTML = 'стоимость машины ' + formatter.format(totalPrice) + ' руб.';
-    } else {
-        document.getElementById('totalPrice').innerHTML = 'стоимость машины 0 руб.';
-    }
+  //provisional price
+  //if provisional price is < 0 (example, too many km -> shows 0)
+  if (totalPrice > 0) {
+    document.getElementById("totalPrice").innerHTML =
+      "стоимость машины " + formatter.format(totalPrice) + " руб.";
+  } else {
+    document.getElementById("totalPrice").innerHTML = "стоимость машины 0 руб.";
+  }
 }
 
-// Чтобы пересчитывала и показывала промежуточную цену сразу и по ходу, если меняются селекты и инпуты
+//evolution of provisional price (price change when inputs and selects are changed)
 for (const input of inputs) {
-    input.addEventListener('input', function () {
-        calculate();
-    })
+  input.addEventListener("input", function () {
+    calculate();
+  });
 }
 
 for (const select of selects) {
-    select.addEventListener('select', function () {
-       calculate();
-    })
+  select.addEventListener("select", function () {
+    calculate();
+  });
 }
 
-// Чтобы не удалять красивую кнопку -> при нажатии выводит результат в окошко
-let showResult = () =>{
-    document.getElementById('formImage').classList.add ('form__invisible');
-    document.getElementById('formResult').classList.remove ('form__invisible');
-    document.getElementById('finalResult').innerHTML = document.getElementById('totalPrice').innerHTML;
-    document.getElementById('totalPrice').innerHTML = '';
-}
-
-
+//result showing
+let showResult = () => {
+  document.getElementById("formImage").classList.add("form__invisible");
+  document.getElementById("formResult").classList.remove("form__invisible");
+  document.getElementById("finalResult").innerHTML =
+    document.getElementById("totalPrice").innerHTML;
+  document.getElementById("totalPrice").innerHTML = "";
+};
